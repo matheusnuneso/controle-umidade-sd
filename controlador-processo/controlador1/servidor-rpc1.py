1282bf9e86e03fc924260613dc789fac7e5f8477
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2023-11-14 15:32:34 -0300

    tratando falhas do servidore ao conectar com o bd

--- controlador-processo/controlador1/servidor-rpc1.py
+++ controlador-processo/controlador1/servidor-rpc1.py
@@ -3,50 +3,123 @@ from rpyc.utils.server import ThreadedServer
 import rpyc
 import psycopg2
 
+timeout_conexao_bd = 1
+
 # Parâmetros de conexão com o bd
 dbname = 'postgres'
 user = 'postgres'
 password = '123456'
 host = 'localhost' 
-port = '5432'
+port_bd1 = '5432'
+port_bd2 = '5433'
 
-conn = psycopg2.connect(f"dbname='{dbname}' user='{user}' password='{password}' host='{host}' port='{port}'")
-cur = conn.cursor()
+string_connetion_bd1 = f"dbname='{dbname}' user='{user}' password='{password}' host='{host}' port='{port_bd1}' connect_timeout='{timeout_conexao_bd}'"
+string_connetion_bd2 = f"dbname='{dbname}' user='{user}' password='{password}' host='{host}' port='{port_bd2}' connect_timeout='{timeout_conexao_bd}'"
 
 class Servidor1(rpyc.Service):
 
-    def exposed_retorna_ultimo_id(self):
-        select_query = 'SELECT MAX(id) FROM umidade_terra;'
-        cur.execute(select_query)
-
-        return cur.fetchone()[0]
-    
-    # retorna os dados entre o id_inicio e id_fim 
-    def exposed_retorna_dados_perdidos(self, id_inicio, id_fim):
-        select_query = f'SELECT data_hora, umidade, molhou FROM umidade_terra WHERE id BETWEEN {id_inicio} AND {id_fim};'
-        cur.execute(select_query)
-        dados_perdidos = [(data_hora.strftime("%Y-%m-%d %H:%M:%S"), umidade, molhou) for data_hora, umidade, molhou in cur.fetchall()]
-
-        return dados_perdidos
-
     def exposed_retorna_todos_dados(self):
-        select_query = 'SELECT data_hora, umidade, molhou FROM umidade_terra;'
-        cur.execute(select_query)
-        dados = [(data_hora.strftime("%Y-%m-%d %H:%M:%S"), umidade, molhou) for data_hora, umidade, molhou in cur.fetchall()]
+        select_query = 'SELECT data_hora, umidade, molhou FROM umidade_terra ORDER BY data_hora DESC;'
+
+        conn = None
+        conn2 = None
+
+        dados = False
+
+        try:
+            conn = psycopg2.connect(string_connetion_bd1)
+            cur = conn.cursor()
+            cur.execute(select_query)
+            dados = [(data_hora.strftime("%Y-%m-%d %H:%M:%S"), umidade, molhou) for data_hora, umidade, molhou in cur.fetchall()]
+
+        except psycopg2.OperationalError:
+            print('BANCO 1 fora')
+
+            try:
+                conn2 = psycopg2.connect(string_connetion_bd2)
+                cur2 = conn2.cursor()
+                cur2.execute(select_query)
+                dados = [(data_hora.strftime("%Y-%m-%d %H:%M:%S"), umidade, molhou) for data_hora, umidade, molhou in cur.fetchall()]
+
+            except psycopg2.OperationalError:
+                print('BANCO 2 fora')
+
+            finally:
+                if(conn2):
+                    conn2.close()
+        
+        finally:
+            if(conn):
+                conn.close()
+
         return dados
 
     def exposed_altera_limiar(self, novo_limiar):
-        update_query = f"UPDATE limiar SET limiar = {novo_limiar};"
-        cur.execute(update_query)
-        conn.commit()
+        query = f"UPDATE limiar SET limiar = {novo_limiar};"
+        
+        conn = None
+        try:
+            conn = psycopg2.connect(string_connetion_bd1, connect_timeout=timeout_conexao_bd)
+            cur = conn.cursor()
 
-    def exposed_retorna_limiar(self):
-        select_query = 'SELECT * FROM limiar;'
-        cur.execute(select_query)
-        return cur.fetchone()[0]
+            cur.execute(query)
+            conn.commit()
+
+        except psycopg2.OperationalError as e:
+            print('BANCO 1 fora')
+
+        finally:
+            if(conn):
+                conn.close()
+
+        conn2 = None
+        try:
+            conn2 = psycopg2.connect(string_connetion_bd2, connect_timeout=timeout_conexao_bd)
+            cur2 = conn2.cursor()
+
+            cur2.execute(query)
+            conn2.commit()
+
+        except psycopg2.OperationalError as e:
+            print('BANCO 2 fora')
+
+        finally:
+            if(conn2):
+                conn2.close()
     
     def exposed_ta_vivo(self):
         return True
+    
+    def executa_query(query):
+        conn = None
+        try:
+            conn = psycopg2.connect(string_connetion_bd1, connect_timeout=timeout_conexao_bd)
+            cur = conn.cursor()
 
+            cur.execute(query)
+            conn.commit()
+
+        except psycopg2.OperationalError as e:
+            print('BANCO 1 fora')
+
+        finally:
+            if(conn):
+                conn.close()
+
+        conn2 = None
+        try:
+            conn2 = psycopg2.connect(string_connetion_bd2, connect_timeout=timeout_conexao_bd)
+            cur2 = conn2.cursor()
+
+            cur2.execute(query)
+            conn2.commit()
+
+        except psycopg2.OperationalError as e:
+            print('BANCO 2 fora')
+
+        finally:
+            if(conn2):
+                conn2.close()
+    
 t = ThreadedServer(Servidor1, port=18861)
 t.start()
